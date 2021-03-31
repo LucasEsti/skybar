50f3a1826bcb03fcd4d34ddb2ba7b51abb59d888
LucasEsti <lie.lucas.razafindrambo@esti.mg>
2021-03-31 11:53:32 +0300

    test mobile device

--- single-produits.php
+++ single-produits.php
@@ -6,8 +6,9 @@
 
 <head>
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name = "viewport" id = "viewport_device">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
     <title><?php the_title(); ?></title>
     <meta content="" name="description">
     <meta content="" name="keywords">
